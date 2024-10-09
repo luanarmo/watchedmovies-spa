d084706b4955b8f355c40210621c251c58d4bdd1
luanarmo <luisangel_arroyo@outlook.com>
2024-10-09 11:47:20 -0600

    fix: alto responsivo para evitar que la card se vea muy estirada aunque la imagen a veces no cubra bien

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -12,7 +12,7 @@ export function ListOfMovies({ movies }) {
                         <img
                             src={movie.poster_url}
                             alt={movie.title}
-                            className="w-full h-[200px] object-cover"
+                            className="w-full h-auto object-cover"
                             loading='lazy'
                         />
                     </Link>
