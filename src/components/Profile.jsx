fd9b278099bafa53f92e8bb11b031b96f9d256f7
luanarmo <luisangel_arroyo@outlook.com>
2024-10-09 17:28:51 -0600

    refactor: Cambio del idioma de mensajes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -17,16 +17,16 @@ export default function Profile() {
             {loading ? <div className='bg-gradient-to-r from-slate-700 to-slate-500 text-white'><Loading /></div> : (
                 <div className='flex flex-col lg:flew-row p-4 gap-2 justify-start items-center h-screen bg-slate-950 text-white'>
                     <h1 className='text-4xl '><strong>{
-                        profile.name ? profile.name : 'Perfil'
+                        profile.name ? profile.name : 'Profile'
                     }</strong></h1>
                     <section>
-                        <h2 className='text-2xl'><strong>Correo:</strong></h2>
+                        <h2 className='text-2xl'><strong>Email:</strong></h2>
                         <p>{profile.email}</p>
 
-                        <h2 className='text-2xl'><strong>Biografia:</strong></h2>
+                        <h2 className='text-2xl'><strong>Bio:</strong></h2>
                         <p>{profile.profile.bio}</p>
 
-                        <h2 className='text-2xl'><strong>Fecha de nacimiento:</strong></h2>
+                        <h2 className='text-2xl'><strong>Birth date:</strong></h2>
                         <p>{profile.profile.birth_date}</p>
                     </section>
                 </div>
