3774b1eb02162bad525810c4ec3c7439a31a7a5c
luanarmo <luisangel_arroyo@outlook.com>
2024-10-09 17:28:18 -0600

    refactor: Cambio del idioma de mensajes

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -26,11 +26,11 @@ export function HomeSearch() {
 
     return (
         <section className="flex flex-col items-start gap-2 p-4  bg-gradient-to-r from-indigo-700 to-indigo-400 text-white">
-            <h1 className="text-4xl font-bold mb-4 ">¡Bienvenido!</h1>
+            <h1 className="text-4xl font-bold mb-4 ">Welcome!</h1>
             <form className="flex w-full gap-2" onSubmit={handleSubmit}>
                 <input
                     type="text"
-                    placeholder="Busca una película ..."
+                    placeholder="Search a movie ..."
                     onChange={handleChange}
                     className="grow  sm:w-auto p-2 border text-black border-gray-300 rounded-sm focus:outline-none focus:ring-2 focus:ring-indigo-400"
                 />
@@ -38,7 +38,7 @@ export function HomeSearch() {
                     type="submit"
                     className="flex-shrink-0 sm:w-auto bg-indigo-700 p-2 rounded-sm hover:bg-indigo-800"
                 >
-                    Buscar
+                    Search
                 </button>
             </form>
         </section>
