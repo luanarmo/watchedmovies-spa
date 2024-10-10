a014894699b79c7b2ff98fe6fb52aa98c9cfbe72
luanarmo <luisangel_arroyo@outlook.com>
2024-10-10 11:03:35 -0600

    refactor: Reutilizacion de componente Search

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import { useContext, useEffect } from 'react'
 import { SearchContext } from '../context/search.jsx'
+import { Search } from './Search.jsx'
 
 export function HomeSearch() {
 
@@ -25,22 +26,12 @@ export function HomeSearch() {
 
 
     return (
-        <section className="flex flex-col items-start gap-2 p-4  bg-gradient-to-r from-indigo-700 to-indigo-400 text-white">
+        <Search
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+        >
             <h1 className="text-4xl font-bold mb-4 ">Welcome!</h1>
-            <form className="flex w-full gap-2" onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Search a movie ..."
-                    onChange={handleChange}
-                    className="grow  sm:w-auto p-2 border text-black border-gray-300 rounded-sm focus:outline-none focus:ring-2 focus:ring-indigo-400"
-                />
-                <button
-                    type="submit"
-                    className="flex-shrink-0 sm:w-auto bg-indigo-700 p-2 rounded-sm hover:bg-indigo-800"
-                >
-                    Search
-                </button>
-            </form>
-        </section>
+        </Search>
+
     )
 }
