b09845e00e79f57b6e44c9a2f12cdc4d372458d0
luanarmo <luisangel_arroyo@outlook.com>
2024-10-09 20:35:51 -0600

    feat: se agregan iconos

--- src/components/WatchedMovie.jsx
+++ src/components/WatchedMovie.jsx
@@ -1,23 +1,34 @@
-import { FaTrash } from 'react-icons/fa';
+import { FaTrash, FaStar, FaCalendar, FaEye } from 'react-icons/fa';
 import { Link } from 'react-router-dom'
 
 export const WatchedMovie = ({ movie, onDelete }) => {
     return (
         <div className="flex rounded-xl bg-slate-600">
             <Link to={`/watchedDetails/${movie.id}`}>
-                <div className='md:shrink-0'>
+                <div className='w-32'>
                     <img
                         src={movie.poster_url}
                         alt={movie.title}
-                        className="h-48 w-32 object-cover rounded-l-xl"
+                        className="object-cover rounded-l-xl"
                     />
                 </div>
             </Link>
             <div className="p-2">
                 <Link to={`/watchedDetails/${movie.id}`}>
                     <h2 className="text-xl font-bold">{movie.title}</h2>
-                    <p className='text-gray-500'>{movie.release_date}</p>
                 </Link>
+                <div className='flex items-center gap-2'>
+                    <FaCalendar />
+                    <p>{movie.release_date}</p>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <FaEye />
+                    <p>{movie.total_views}</p>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <FaStar className='text-yellow-400' />
+                    <p>{movie.average_rating}</p>
+                </div>
                 <div className="mt-4 space-x-4">
                     <button
                         onClick={() => onDelete(movie.id)}
