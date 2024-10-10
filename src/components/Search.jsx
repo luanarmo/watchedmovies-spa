a655329785e5386500e0cd99f76db9768378cf62
luanarmo <luisangel_arroyo@outlook.com>
2024-10-10 06:31:35 -0600

    feat: cambio del idioma a la busqueda avanzada

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ export function Search({ onChange, onSubmit, search, error }) {
             <form className="container flex items-center justify-center space-x-4" onSubmit={onSubmit}>
                 <input
                     type="text"
-                    placeholder="Busca una película ..."
+                    placeholder="Search a movie ..."
                     onChange={onChange}
                     value={search}
                     className="flex-grow  sm:w-auto p-2 border text-black border-gray-300 rounded-sm focus:outline-none focus:ring-2 focus:ring-indigo-400"
@@ -15,7 +15,7 @@ export function Search({ onChange, onSubmit, search, error }) {
                     type="submit"
                     className="flex-shrink-0 sm:w-auto bg-indigo-700 p-2 rounded-sm hover:bg-indigo-800"
                 >
-                    Buscar
+                    Search
                 </button>
             </form>
         </section>
