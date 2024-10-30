86fd1e0b794f7625c09b133edb1e5ff8abf9426d
luanarmo <luisangel_arroyo@outlook.com>
2024-10-30 16:44:36 -0600

    feat: Implementar estados de carga

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -4,13 +4,16 @@ import { popularMovies } from '../services/popular.js'
 export const useMovies = () => {
     // Custom hook to fetch popular movies
 
+    const [loading, setLoading] = useState(true)
     const [popular, setPopular] = useState([])
 
 
     const getPopularMovies = useCallback(async () => {
+        setLoading(true)
         const newMovies = await popularMovies()
         setPopular(newMovies)
+        setLoading(false)
     }, [])
 
-    return { popular, getPopularMovies }
+    return { popular, getPopularMovies, loading }
 }
