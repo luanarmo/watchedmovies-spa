6281c9d05a634a75026e9d19705f4223f31a0fbc
luanarmo <luisangel_arroyo@outlook.com>
2024-10-30 16:47:46 -0600

    feat: Implementar esqueleto para lista de peliculas vistas

--- src/components/WatchedList.jsx
+++ src/components/WatchedList.jsx
@@ -1,7 +1,7 @@
 import { Base } from './Base.jsx'
 import { useWatched } from '../hooks/useWatched.js'
-import { Loading } from './Loading.jsx'
 import { WatchedMovie } from './WatchedMovie.jsx'
+import { WatchedMovieSkeleton } from './watchedMovieSkeleton.jsx'
 import { useEffect } from 'react'
 
 export default function Watched() {
@@ -10,11 +10,7 @@ export default function Watched() {
 
     useEffect(() => {
         fetchWatched()
-    }, [])
-
-    if (loading) {
-        return <Loading />
-    }
+    }, [fetchWatched])
 
     if (error) {
         return <div className='text-white'>{error}</div>
@@ -23,9 +19,14 @@ export default function Watched() {
     return (
         <Base>
             <div className='flex flex-col gap-2 p-2 bg-slate-950 text-white h-screen overflow-y-auto max-h-[calc(100vh-8rem)]'>
-                {watched.map((movie) => (
-                    <WatchedMovie key={movie.id} movie={movie} onDelete={removeWatched} />
-                ))}
+                {loading ? (
+                    Array.from({ length: 10 }).map((_, index) => <WatchedMovieSkeleton key={index} />)
+                ) : (
+                    watched.map((movie) => (
+                        <WatchedMovie key={movie.id} movie={movie} onDelete={removeWatched} />
+                    ))
+                )
+                }
             </div>
         </Base>
     )
