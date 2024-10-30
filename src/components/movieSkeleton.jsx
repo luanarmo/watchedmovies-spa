bdcea8b092333c1e0c4543f32e2b44fcd79518a4
luanarmo <luisangel_arroyo@outlook.com>
2024-10-30 16:47:21 -0600

    feat: Crear esqueleto de pelicula

---
+++ src/components/movieSkeleton.jsx
@@ -0,0 +1,18 @@
+export function MoviesSkeleton() {
+    const skeletonItems = Array.from({ length: 12 }); // Cambia el número según cuántos esqueléticos necesites
+
+    return (
+        <section>
+            <ul className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-8 gap-4 p-4 bg-slate-950">
+                {skeletonItems.map((_, index) => (
+                    <li
+                        key={index}
+                        className="bg-slate-600 text-transparent shadow-lg rounded-lg overflow-hidden animate-pulse"
+                    >
+                        <div className="h-64 bg-gray-300"></div>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+}
