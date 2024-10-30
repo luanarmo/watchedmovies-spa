2f76e0e604658f2af00fe2b58103f36b18b02b50
luanarmo <luisangel_arroyo@outlook.com>
2024-10-30 16:45:13 -0600

    feat: Implementar skeleto de listado de peliculas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,25 @@
 import { HomeSearch } from './HomeSearch'
 import { Base } from './Base'
 import { Movies } from './Movies'
+import { MoviesSkeleton } from './movieSkeleton'
 import { useMovies } from '../hooks/useMovies'
 import { useEffect } from 'react'
 
 export default function Home() {
 
-    const { popular, getPopularMovies } = useMovies()
+    const { popular, getPopularMovies, loading } = useMovies()
 
 
     useEffect(() => {
         getPopularMovies()
-    }, [])
+    }, [getPopularMovies])
 
 
     return (
         <div className="h-screen">
             <Base>
                 <HomeSearch />
-                <Movies movies={popular} />
+                {loading ? <MoviesSkeleton /> : <Movies movies={popular} />}
             </Base>
         </div>
     )
