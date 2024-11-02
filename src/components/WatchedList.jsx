27a5ea84412fa97c743e7f974e307e65d5b6d2a8
luanarmo <luisangel_arroyo@outlook.com>
2024-11-02 16:12:45 -0600

    fix: Corregir validacion de tiempo de token

--- src/components/WatchedList.jsx
+++ src/components/WatchedList.jsx
@@ -9,17 +9,21 @@ import { useNavigate } from 'react-router-dom'
 
 export default function Watched() {
 
-    const { isExpired } = useContext(SesionContext)
+    const { sesion } = useContext(SesionContext)
     const { watched, loading, error, removeWatched, fetchWatched } = useWatched()
 
     const navigate = useNavigate()
 
     useEffect(() => {
+        const isExpired = () => {
+            return sesion.expiresAt ? Date.now() > sesion.expiresAt : true;
+        }
+
         if (isExpired()) {
             navigate('/login')
         }
         fetchWatched()
-    }, [fetchWatched, isExpired, navigate])
+    }, [])
 
     if (error) {
         return <div className='text-white'>{error}</div>
