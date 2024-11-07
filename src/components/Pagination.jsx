f21c8409379709e6ac98ffe332d3edd9dbd4a3f0
luanarmo <luisangel_arroyo@outlook.com>
2024-11-07 12:46:12 -0600

    fix: Corregir componente de paginacion

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,28 +1,35 @@
-export const Pagination = ({ page, handlePage }) => {
+export const Pagination = ({ page, pagination, handlePage }) => {
     return (
-        <div className="flex items-center justify-center rounded-full bg-gray-200 p-1">
+        <div className="flex items-center justify-center rounded-full bg-gray-800 p-2 space-x-4">
             <button
-                className={'px-4 py-2 rounded-full transition-colors text-gray-700 hover:bg-gray-300'}
+                className={`px-4 py-2 rounded-full text-white transition-colors ${page <= 1 ? 'bg-gray-600 cursor-not-allowed' : 'bg-gray-700 hover:bg-gray-600'}`}
                 onClick={() => {
-                    const newPage = page - 1
-                    if (newPage < 1) return
-                    handlePage(newPage)
+                    const newPage = page - 1;
+                    if (newPage < 1) return;
+
+                    if (pagination?.previous) {
+                        handlePage(newPage);
+                    }
                 }}
+                disabled={page <= 1}
             >
                 Before
             </button>
-            <p className='text-xl p-2'>
+            <p className="text-xl text-white">
                 {page}
             </p>
             <button
-                className={'x-4 py-2 rounded-full transition-colors text-gray-700 hover:bg-gray-300'}
+                className="px-4 py-2 rounded-full text-white transition-colors bg-gray-700 hover:bg-gray-600"
                 onClick={() => {
-                    const newPage = page + 1
-                    handlePage(newPage)
+                    const newPage = page + 1;
+                    if (pagination?.next) {
+                        handlePage(newPage);
+                    }
                 }}
+                disabled={pagination?.next === null}
             >
                 Next
             </button>
         </div>
     );
-}
+};
