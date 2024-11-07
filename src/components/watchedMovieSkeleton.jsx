858c0523e9176919dfc3757608217246c4927485
luanarmo <luisangel_arroyo@outlook.com>
2024-11-07 12:49:12 -0600

    fix: Cambiar el esqueleto de peliculas vistas

--- src/components/watchedMovieSkeleton.jsx
+++ src/components/watchedMovieSkeleton.jsx
@@ -1,27 +1,14 @@
 export const WatchedMovieSkeleton = () => {
     return (
-        <div className="flex rounded-xl bg-slate-600 animate-pulse">
-            <div className='w-32'>
-                <div className="bg-gray-300 h-full rounded-l-xl"></div>
-            </div>
+        <li className="flex flex-col rounded-xl bg-slate-600 animate-pulse">
+            <div className="bg-gray-300 h-20 w-full mb-2 rounded-xl"></div>
             <div className="p-2">
-                <div className="bg-gray-300 h-6 w-3/4 rounded mb-2"></div>
-                <div className="flex items-center mb-1">
-                    <div className="bg-gray-300 h-4 w-1/4 rounded mr-2"></div>
+                <div className="flex items-center justify-center gap-2">
                     <div className="bg-gray-300 h-4 w-1/4 rounded"></div>
-                </div>
-                <div className="flex items-center mb-1">
-                    <div className="bg-gray-300 h-4 w-1/4 rounded mr-2"></div>
                     <div className="bg-gray-300 h-4 w-1/4 rounded"></div>
-                </div>
-                <div className="flex items-center mb-1">
-                    <div className="bg-gray-300 h-4 w-1/4 rounded mr-2"></div>
                     <div className="bg-gray-300 h-4 w-1/4 rounded"></div>
-                </div>
-                <div className="mt-4 space-x-4">
-                    <button className="bg-gray-300 rounded p-2 w-12 h-8"></button>
                 </div>
             </div>
-        </div>
+        </li>
     );
 };
