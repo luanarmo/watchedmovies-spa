e79611476abc92a97aedc87dcf7db6b1b4c033ee
luanarmo <luisangel_arroyo@outlook.com>
2024-11-17 00:06:12 -0600

    feat: crear nuevo servicio

--- src/services/watchedMoviesServices.js
+++ src/services/watchedMoviesServices.js
@@ -36,6 +36,31 @@ export const getWatched = async ({ access, page }) => {
 
 }
 
+export const getPosters = async ({ access }) => {
+    // Fetch watched movies from the API
+
+    try {
+        const response = await fetch(`${BASE_API_URL}/api/watched-movies/posters/`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${access}`
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error('Error fetching the image');
+        }
+
+        // Retornar el Blob directamente
+        return await response.blob();
+
+    }
+    catch (e) {
+        throw new Error(`Error fetching watched movies ${e}`)
+    }
+
+}
+
 export const addWatched = async ({ movie, payload, access }) => {
     // remove poster_url and backdrop_url from the movie object
 
