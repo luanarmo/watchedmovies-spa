772dc2edda3eb8d64a975040317afdd15e027407
luanarmo <luisangel_arroyo@outlook.com>
2024-11-17 18:04:44 -0600

    feat: Implementar filtros a la generacion de imagenes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -25,9 +25,9 @@ export default function Profile() {
         fetchYears();
     }, []);
 
-    const handleGenerateImage = () => {
-        console.log(`Generating image for year`);
-        fetchPoster();
+    const handleGenerateImage = (payload) => {
+
+        fetchPoster(payload);
     };
 
     return (
@@ -58,20 +58,35 @@ export default function Profile() {
                     <form
                         className="bg-slate-800 p-4 rounded-lg shadow-lg w-full max-w-lg flex flex-col gap-4"
                         onSubmit={(e) => {
-                            e.preventDefault();
-                            handleGenerateImage();
+                            e.preventDefault()
+                            const formData = new FormData(e.target)
+                            const payload = Object.fromEntries(formData)
+                            handleGenerateImage(payload)
                         }}
                     >
                         <label htmlFor="year" className="text-lg font-medium">
-                            Select Year
+                            Select Year:
                         </label>
-                        <select name="" id=""
+
+                        <select name="year" id="year"
                             className="w-full px-3 py-2 rounded-lg bg-slate-700 text-white border border-slate-600 focus:outline-none focus:ring-2 focus:ring-slate-500"
                         >
                             {years.map((year) => (
                                 <option key={year} value={year}>{year}</option>
                             ))}
                         </select>
+
+                        <label htmlFor="order" className="text-lg font-medium">
+                            Order by:
+                        </label>
+
+                        <select name="order" id="order"
+                            className="w-full px-3 py-2 rounded-lg bg-slate-700 text-white border border-slate-600 focus:outline-none focus:ring-2 focus:ring-slate-500"
+                        >
+                            <option value="-first_watched_date">Watched date (desc)</option>
+                            <option value="first_watched_date">Watched date (asc)</option>
+                        </select>
+
                         <button
                             type="submit"
                             className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-500 transition"
