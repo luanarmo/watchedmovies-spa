c09c9c93b718fdf6967477298de414e846f2cb0c
luanarmo <luisangel_arroyo@outlook.com>
2025-03-05 21:41:58 -0600

    feat: Aplica configuracion para componente watchedmovie

--- src/components/WatchedList.jsx
+++ src/components/WatchedList.jsx
@@ -97,6 +97,7 @@ export default function Watched() {
                                 key={movie.id}
                                 movie={movie}
                                 onDelete={removeWatched}
+                                redirect={'watchedDetails'}
                             />
                         ))}
                     </ul>
